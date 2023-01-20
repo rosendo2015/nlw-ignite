9626ff3f540639f24175c5149d4f2c11c6ae89a0
rosendo <rosendc30@gmail.com>
2023-01-20 14:51:56 -0300

    web 3 parte

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,37 +1,95 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
-export function NewHabitForm(){
-    return(
-        <form className="w-full flex  flex-col mt-6">
-            <label 
-            className="font-semibold leading-tight"
-            htmlFor="title"
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
+export function NewHabitForm() {
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  async function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+    
+if(!title || weekDays.length === 0){
+  return
+}
+await api.post('habits', {
+  title, 
+  weekDays,
+})
+
+setTitle('')
+setWeekDays([])
+
+alert('Habito cadastrado com sucesso!')
+}
+
+  function hrandleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDayRemovedOneDay = weekDays.filter((day) => day !== weekDay);
+      setWeekDays(weekDayRemovedOneDay);
+    } else {
+      const weekDayAddOneDay = [...weekDays, weekDay];
+      setWeekDays(weekDayAddOneDay);
+    }
+  }
+
+  return (
+    <form onSubmit={createNewHabit} className="w-full flex  flex-col mt-6">
+      <label className="font-semibold leading-tight" htmlFor="title">
+        Qual seu comprometimento?
+      </label>
+      <input
+        type="text"
+        id="title"
+        placeholder="ex.: Exerceicios, dormir bem, etc..."
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        autoFocus
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <label htmlFor="" className="font-semibold leading-tight mt-4">
+        Qual a recorrência?
+      </label>
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeekDays.map((weekDay, index) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className="flex items-center gap-3"
+              checked={weekDays.includes(index)}
+              onCheckedChange={() => {
+                hrandleToggleWeekDay(index);
+              }}
             >
-                Qual seu comprometimento?
-            </label>
-            <input 
-            type="text"
-            id="title"
-            placeholder="ex.: Exerceicios, dormir bem, etc..."
-            className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-            autoFocus
-            />
-
-            <label htmlFor="" className="font-semibold leading-tight mt-4">
-                Qual a recorrência?
-            </label>
-
-            <button 
-            type="submit"
-            className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
-            >
-                <Check 
-                size={20}
-                weight="bold"
-                />
-                Confirmar
-            </button>
-
-        </form>
-    )
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800  group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="text-white leading-tight">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+      >
+        <Check size={20} weight="bold" />
+        Confirmar
+      </button>
+    </form>
+  );
 }
